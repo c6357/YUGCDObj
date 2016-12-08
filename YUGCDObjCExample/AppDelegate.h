268befeba359fc2ae79e6210c8142a9841fdb38b
yuzhenxiang <yuzhenxiang@hme.cn>
2016-12-08 14:35:33 +0800

    init

---
+++ YUGCDObjCExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YUGCDObjCExample
+//
+//  Created by BruceYu on 16/12/8.
+//  Copyright © 2016年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YUGCDObjCExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YUGCDObjCExample
+//
+//  Created by BruceYu on 16/12/8.
+//  Copyright © 2016年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
